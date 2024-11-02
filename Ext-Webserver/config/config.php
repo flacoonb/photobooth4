773fa445d398f7c883c89e3a5787ce2618071102
flacoonb <44100356+flacoonb@users.noreply.github.com>
2024-11-02 22:34:44 +0100

    Create config.php

---
+++ Ext-Webserver/config/config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'base_url' => 'https://XXXX', // Basis-URL der Website
+    'photobooth_webhook_url' => 'https://XXXX/private/webhook/webhook_receiver.php' // URL der Photobooth
+];
+?>
