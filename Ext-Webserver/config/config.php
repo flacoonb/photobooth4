4adccb2802b05317d19712421f4da7adaf1eac08
flacoonb <44100356+flacoonb@users.noreply.github.com>
2024-11-03 16:31:46 +0100

    Update config.php

--- Ext-Webserver/config/config.php
+++ Ext-Webserver/config/config.php
@@ -2,5 +2,6 @@
 return [
     'base_url' => 'https://XXXX', // Basis-URL der Website
     'photobooth_webhook_url' => 'https://XXXX/private/webhook/webhook_receiver.php' // URL der Photobooth
+    'delete_image_webhook_url' => 'https://fotomat-sg.ch/test/delete_image.php' // Webhook für die Bildlöschung
 ];
 ?>
