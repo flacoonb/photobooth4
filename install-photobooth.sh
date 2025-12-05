9053723f61709593dc4b9d96f9ca1e4de4ee4538
Photobooth <Photobooth@localhost>
2025-12-05 21:36:01 +0000

    Fix: whiptail TTY requirement
    - whiptail benötigt TTY auf stdout für korrekte Darstellung
    - Leite stdout auf fd 4 (original TTY) um
    - Nutze --output-fd 3 um Ergebnis an fd 3 (Kopie von original stdout/pipe) zu senden
    - Behebt 'nichts passiert' Fehler (blank screen)

--- install-photobooth.sh
+++ install-photobooth.sh
@@ -138,9 +138,9 @@ function log() {
 
 function whiptail_wrapper() {
     # Redirect whiptail UI to original stderr (fd 5)
-    # Redirect whiptail result to current stdout (fd 1)
-    # This works for both capturing output $(...) and normal display
-    whiptail "$@" --output-fd 1 2>&5
+    # Redirect whiptail stdout to original stdout (fd 4) to satisfy TTY requirement
+    # Redirect result to fd 3, which is a copy of current stdout (pipe or logfile)
+    whiptail "$@" --output-fd 3 1>&4 2>&5 3>&1
 }
 
 function progress_init() {
